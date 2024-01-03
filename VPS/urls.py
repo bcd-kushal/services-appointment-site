76e16e12116efd36e781c1480aed85188ebdf96d
Kushal <am.kushal02@gmail.com>
2024-01-03 23:54:01 +0530

    Add files via upload

--- VPS/urls.py
+++ VPS/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('date-pick/', app.goto_datepick, name='goto-datepick'),
     path('date-pick/<int:id>/', app.datepick, name='datepick'),
     path('payment/', app.payment, name='payment'),
-    path('payment/', app.payment, name='payment'),
 
-    path('payment/', app.process_payment, name='process-payment'),
+    path('payment-process/', app.process_payment, name='process-payment'),
     
 ]
